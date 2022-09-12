e64107acf3ec5f77be596175a6b2c6893d571f2d
Joseph50079 <josephogbah2019@gmail.com>
2022-09-12 12:38:59 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function
+ * description - print numbers in base 10 with putchar
+ * Return: return 0
+ */
+int main(void)
+{
+
+	int me;
+
+
+	for (me = '0'; me <= '9'; me++)
+	{
+	putchar(me);
+
+		if (me != 9)
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
