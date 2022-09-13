dfb42385dd09a755e26dce1b3b440bf89497028f
Joseph50079 <josephogbah2019@gmail.com>
2022-09-13 16:54:47 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * description - print numbers in double digit with putchar
+ * Return: return 0
+ */
+int main(void)
+{
+
+	unsigned int i = 0, n, j;
+
+	while (i <= 9)
+	{
+		n = 1;
+		while (n <= 9)
+		{
+			j = 2;
+			while (j <= 9)
+
+			{
+
+			if (i != n && i < n && n != j && n < j)
+
+			{
+				putchar(i + 48);
+				putchar(n + 48);
+				putchar(j + 48);
+
+				if (i + n + j != 170)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+			++j;
+
+			}
+			++n;
+		}
+		++i;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
