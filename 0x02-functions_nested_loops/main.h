f10a76e1c3da63b022940508e18c6743cd3b8521
Joseph50079 <josephogbah2019@gmail.com>
2022-09-14 13:09:49 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_10x(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_baver(void);
+void times_table(void);
+int ad(int a, int n);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
