a3cac916cc09cc7c13d5e04a28b4c94017242944
Joseph50079 <josephogbah2019@gmail.com>
2022-09-14 14:09:46 -0700

    ammending main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,15 +3,15 @@
 
 int _putchar(char);
 void print_alphabet(void);
-void print_alphabet_10x(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int c);
 int _abs(int);
 int print_last_digit(int);
-void jack_baver(void);
+void jack_bauer(void);
 void times_table(void);
-int ad(int a, int n);
+int add(int a, int n);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
