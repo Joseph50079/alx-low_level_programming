0afc09442145411ebb29e58c9ff16dbd634bbbdd
Joseph50079 <josephogbah2019@gmail.com>
2022-09-14 23:10:41 -0700

    main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - funtion is main
+ * description - print positive or negative int with functions prototype 
+ *Return: 0
+ */
+
+int main(void)
+{
+	int z = 0;
+	
+	positive_or_negative(z);
+
+	return (0);
+}
