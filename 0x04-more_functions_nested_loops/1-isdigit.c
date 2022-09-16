e2210032a7490a3943e5c06cec303daa662bda03
Joseph50079 <josephogbah2019@gmail.com>
2022-09-16 05:12:32 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - function
+ * @c: return 1 if it's a digit
+ * description - 1 if it is a digit
+ * Return: 0
+ */
+
+int _isdigit(int c);
+{
+	int c;
+
+	for (c = "45"; c <= "54";)
+		return (1);
+	else
+		return (0);
+}
