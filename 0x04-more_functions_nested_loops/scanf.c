cc86c8d48fd06efaf7ef6d708f608691a2ecd7b8
Joseph50079 <josephogbah2019@gmail.com>
2022-09-16 06:25:00 -0700

    cdtwaty

---
+++ 0x04-more_functions_nested_loops/scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * description - scanf explanntion
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	printf("what is your age:\n");
+	scanf("%d", &c);
+
+	printf("my age is %d \n", c);
+
+	return (0);
+}
