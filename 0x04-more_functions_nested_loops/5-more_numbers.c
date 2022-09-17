557eb40fbf35f5d4a6270348b7cf63816f4b4341
Joseph50079 <josephogbah2019@gmail.com>
2022-09-17 16:54:13 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - function print numbers in line
+ * description - print numbers in line
+ */
+
+void more_numbers(void)
+{
+	int l = 0, n;
+
+	while (l < '10')
+	{
+		for (l = '0'; l <= "10"; l++)
+
+		n = '0';
+		for (n = 0; n <= 14; n++)
+		{
+			_putchar(n);
+			_putchar('\n');
+		}
+
+	}
+
+	_putchar('\n');
+}
