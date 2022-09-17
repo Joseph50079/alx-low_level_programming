60e03e463d7342fa81c7a969ecac8b91a3087a16
Joseph50079 <josephogbah2019@gmail.com>
2022-09-17 10:24:12 -0700

    2-mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,21 @@
-#include ""
+#include "main.h"
+
+/**
+ * mul - function prototype
+ * @a: it multiplies the other 
+ * @b: it is been multiplied by the other int
+ * description - this function prototype multiplies two integers
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	int a, b;
+
+	int result;
+	
+	if (result = a * b || b * a)
+	{
+	return (result);
+	}
+	return (0);
+}
