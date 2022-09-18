e3eb037335457bd957ffaa2093109cbe33f641ae
Joseph50079 <josephogbah2019@gmail.com>
2022-09-17 17:00:47 -0700

    5-more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@ void more_numbers(void)
 {
 	int l = 0, n;
 
-	while (l < '10')
+	while (l < 10)
 	{
-		for (l = '0'; l <= "10"; l++)
+		for (l = 0; l <= 10; l++)
 
 		n = '0';
-		for (n = 0; n <= 14; n++)
+		for (n = '0'; n <= "14"; n++)
 		{
 			_putchar(n);
 			_putchar('\n');
