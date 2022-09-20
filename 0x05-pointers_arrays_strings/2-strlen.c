228bd743989b729b5ca206480e9da6ab8fde6504
Joseph50079 <josephogbah2019@gmail.com>
2022-09-19 18:19:27 -0700

    string length count

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,15 @@
 /**
  * _strlen - function returns length of string
  * @s: parameter
+ * Return: returns i value in int
  */
 
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0') i++;
+	while (s[i] != '\0')
+	i++;
 	return (i);
 
 
