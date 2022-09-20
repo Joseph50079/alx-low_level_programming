ad63e15f6677fb7140cafb40020d27a0cd4be32e
Joseph50079 <josephogbah2019@gmail.com>
2022-09-19 18:32:13 -0700

    _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function prints a string and a new line
+ * @str: parameter
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	c = 0;
+
+	while (str[c] != '\0')
+	c++;
+}
