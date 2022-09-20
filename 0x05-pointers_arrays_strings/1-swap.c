078ece19e70eaf5f80e6e4f27ace8e68684eeee2
Joseph50079 <josephogbah2019@gmail.com>
2022-09-19 17:21:44 -0700

    swaps an int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - funtion swaps two inter
+ * @a: function parameter
+ * @b: function parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
