d09fd290a1b359088d0697279a25132d1c2241b7
Joseph50079 <josephogbah2019@gmail.com>
2022-09-19 20:52:16 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
 
 void print_rev(char *s)
 {
-	int c;
+	int c, n = 0;
 
-	c = 0;
 
-	while (s[c] != '\0')
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		for (s[c] < '\0'; --c;)
-		{
+		n++;
+	}
+	for (i=c - 1; i >= 0; i--)
+	{
 		putchar(s[c]);
-		}	
 	}
 	putchar('\n');
 }
