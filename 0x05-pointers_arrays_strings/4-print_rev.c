1db3ec38f4b52506d6e12017b108142c01ca59c9
Joseph50079 <josephogbah2019@gmail.com>
2022-09-19 19:24:31 -0700

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - function print in reverse
+ * @s: parameter
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	c = 0;
+	
+	for (s[c] < != '\0'; c--)
+	{
+		putchar(s[c]);
+	}
+	putchar('\n')
+}
