a826f2567e7df34fbb4cc88f028fe5398650f8ce
Joseph50079 <josephogbah2019@gmail.com>
2022-09-22 05:27:40 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _strcat - function concatinate
+ * @dest: dest main string
+ * @src: last string
+ * Return: return dest
+ */
 char *_strcat(char *dest, char *src)
 {
-	if (dest[i] != '\0')
-		dest = dest[i] + src[i]
-	return (dest)
+	int i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	return (dest);
 }
