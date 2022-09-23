c87a6a23218c032bc2d636563b1fb3ee294d6712
Joseph50079 <josephogbah2019@gmail.com>
2022-09-22 17:29:08 -0700

    strcat +

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@ char *_strcat(char *dest, char *src)
 	int i = 0, n;
 	while (*(dest + i) != '\0')
 	i++;
-	
+
 	for (n = 0; *(src + n) != '\0'; n++, i++)
 	{
-		
+
 		*(dest + i) = *(src + n);
 	}
 		dest[i] = '\0';
-	
+
 
 	return (dest);
 
