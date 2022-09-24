0b68a6dbe862a16397b605b951024f356235439c
Joseph50079 <josephogbah2019@gmail.com>
2022-09-24 00:57:18 -0700

    strncat function prototype

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 /**
  * *_strncat - function concatenate with int num
@@ -17,8 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-
-		dest[i];
+		;
 
 	for (j = 0; src[j] != '\0' && j < n; i++, j++)
 	{
