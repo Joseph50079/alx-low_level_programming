04f59bee60c3966bab010b68223b3441a932c305
Joseph50079 <josephogbah2019@gmail.com>
2022-09-28 07:55:39 -0700

    strpbrk and strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strstr - function get the link of a prefixed substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+	{
+		return (haystack);
+	}
+		haystack++;
+	}
+		if (*needle == '\0')
+			return (NULL);
+}
