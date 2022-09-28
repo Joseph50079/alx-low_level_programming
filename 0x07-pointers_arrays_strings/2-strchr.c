356046317330113bee5906bed6057d7be47974ea
Joseph50079 <josephogbah2019@gmail.com>
2022-09-27 20:10:52 -0700

    strspn and strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strchr - function
+ * @s: string
+ * @c: accepted char
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	{
+		return (s);
+	}
+	s++;
+	}
+	return ('\0');
+}
