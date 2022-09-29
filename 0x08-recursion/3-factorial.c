ab2df99df8b41e7a52b005532e72a42baca4d67d
Joseph50079 <josephogbah2019@gmail.com>
2022-09-29 10:06:17 -0700

    1-101

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
   * factorial - get the factorial of a given number
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -26,3 +26,4 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
+}
