2794fac6ddc2f469758c7ebf2a3da3b4dc149cf8
Joseph50079 <josephogbah2019@gmail.com>
2022-09-30 12:07:36 -0700

    argc printed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function with arguments
+ * @argc: counts
+ * @argv: arrays
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%i", argc);
+	return (0);
+}
