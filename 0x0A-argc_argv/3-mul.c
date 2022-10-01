9789363d6dd387eda94cf5755498b89ceca721fb
Joseph50079 <josephogbah2019@gmail.com>
2022-09-30 17:03:28 -0700

    multiplication with main arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - function with arguments
+ * @argc: counts strings
+ * @argv: arrays
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	for (i = 1; i < argc; i++)
+	{
+		int x = atoi(argv[i]);
+		result = result * x;
+		printf("this argv[%d]\n", i);
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
