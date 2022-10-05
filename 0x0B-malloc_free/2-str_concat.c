49cdcfdf63adc60eba48065a92d637ad5d681ac0
Joseph50079 <josephogbah2019@gmail.com>
2022-10-05 11:53:41 -0700

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,27 +11,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = strlen(s1) + strlen(s2);
+	int i;
 	char *t;
 	int j, k, num;
 
+	for (j = 0; s1[j] != '\0'; j++)
+	;
+	for (k = 0; s2[k] != '\0'; k++, j++)
+	{
+	s1[j] = s2[k];
+	}
+	s1[j] = '\0';
+
+	i = strlen(s1) + strlen(s2) + 1;
 	t = malloc(sizeof(char) * i);
 	if (t == NULL)
 	{
 	return (NULL);
 	}
 
-	for (j = 0; j != '\0'; j++)
-		s1[j];
-	for (k = 0; k != '\0'; k++, j++);
+	for (num = 0; num < i; num++)
 	{
-	s1[j] = s2[k];
-	}
-	s1[j] = '\0';
-    
-	for (num = 0; num < s1; num++)
-	{
-	t[num] = s1;
+	t[num] += s1[num];
 	}
 
 	return (t);
