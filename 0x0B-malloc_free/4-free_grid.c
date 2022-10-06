4501227419e21d22a6ee7ffba1650f3b0edaab89
Joseph50079 <josephogbah2019@gmail.com>
2022-10-05 20:14:34 -0700

    free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,11 +13,12 @@ void free_grid(int **grid, int height)
 	int i;
 
 	if (grid == NULL || height <= 0)
-		return (1);
+		return;
 
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
 	free(grid);
+	exit(EXIT_SUCCESS);
 }
