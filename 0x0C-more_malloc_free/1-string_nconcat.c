d15c13c08a39364cb3ee1da014d6d47a07be259c
Joseph50079 <josephogbah2019@gmail.com>
2022-10-07 21:29:30 -0700

    1 2 3 c file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *strin_nconcat - function
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number set to s2
+ * Return: ptr
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j = 0, c1 = strlen(s1);
+	unsigned int sum = strlen(s1) + strlen(s2) + 1;
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * sum);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < c1 && s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (; i < sum && s2[j] != '\0'; i++)
+	{
+		if (j < n)
+		{
+			ptr[i] = s2[j];
+		}
+		j++;
+	}
+	ptr[sum - 1] = '\0';
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *array_range - function
+ * @min: minimum
+ * @max: maximum
+ * Return: ptr
+ */
+
+int *array_range(int min, int max)
+{
+	int mi, ma;
+	int *ptr;
+	int i;
+
+	mi = min;
+	ma = max;
+	ptr = malloc(sizeof(int) * ma);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (mi > ma)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ma; i++)
+	{
+		if (mi < ma)
+		{
+			ptr[i] = mi;
+		}
+		mi++;
+	}
+	return (ptr);
+}
