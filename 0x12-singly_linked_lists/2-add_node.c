a10f2095a4f7752b11023483e34e817007100ebf
Joseph50079 <josephogbah2019@gmail.com>
2022-10-25 07:35:30 -0700

    commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_node - add more node
+ * @head: head pointer
+ * @str: string
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int n = _strlen((char *)str);
+
+	list_t *ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->str = strdup(str);
+	ptr->len = n;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
