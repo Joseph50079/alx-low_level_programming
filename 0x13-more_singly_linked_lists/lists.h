685654ca97fd216077508615c4c52e54458dd5b0
Joseph50079 <josephogbah2019@gmail.com>
2022-10-25 19:47:36 -0700

    betty lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
