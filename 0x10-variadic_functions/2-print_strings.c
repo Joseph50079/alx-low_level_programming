8ba927ebfb250df964cad318bb792c9a93c19ee3
Joseph50079 <josephogbah2019@gmail.com>
2022-11-22 20:02:54 -0800

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_funtion.h"
+
+
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		char *x = va_arg(arg, char *);
+		if (!separator)
+		{
+			printf("%s", x);
+		}
+		else
+		{
+			if (x == NULL)
+			{
+				x = "(nil)";
+			}
+			printf("%s", x);
+			if (i < (n - 1))
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	_putchar('\n');
+	va_end(arg);
+}
