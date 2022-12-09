ee4f07eac6a2747beb5c2c09ae91bedd62bf1364
Joseph50079 <josephogbah2019@gmail.com>
2022-12-08 16:29:31 -0800

    print dlistint

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "list.h"
+
+/**
+ * print_dlistint - function print dlistint
+ * @h: head
+ * Return: i
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+	dlistint_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
