f42c7c245eeb1a18be259ed8589b312b917349ad
Joseph50079 <josephogbah2019@gmail.com>
2022-12-08 18:10:43 -0800

    add node int

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,12 +20,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		*head = ptr;
 	}
+	temp->n = n;
 	if (*head == NULL)
 	{
 		temp->prev = NULL;
 		*head = temp;
+		return (temp);
 	}
-	temp->n = n;
 	while (ptr != NULL)
 	{
 		ptr = ptr->next;
