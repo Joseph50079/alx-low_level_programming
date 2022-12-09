d53e82b86460a277fbe5cfc781f5885a2e61df34
Joseph50079 <josephogbah2019@gmail.com>
2022-12-08 20:18:49 -0800

    inserted

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	
+
 	for (; idx != 1; idx--)
 	{
 		ptr = ptr->next;
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	
+
 	temp->n = n;
 	temp->prev = ptr;
 	temp->next = ptr->next;
