287960564cde0853823f9a43357d091c29d01883
Joseph50079 <josephogbah2019@gmail.com>
2022-12-08 20:14:58 -0800

    inserted

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function add node at index
+ * @idx: index where data (n) will be added
+ * @h: head pointer
+ * @n: data to add
+ * Return: temp
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr = *h, *temp = malloc(sizeof(dlistint_t));
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	
+	for (; idx != 1; idx--)
+	{
+		ptr = ptr->next;
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+	}
+	if (ptr->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	
+	temp->n = n;
+	temp->prev = ptr;
+	temp->next = ptr->next;
+	ptr->next->prev = temp;
+	ptr->next = temp;
+
+	return (temp);
+}
