36a35bf1b8a8edabed32a17fb7362c2376ff8731
Joseph50079 <josephogbah2019@gmail.com>
2023-06-20 16:27:31 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,6 +43,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 	ptr = (dog_t *)malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
