5d3b890e788004d6a48dfe3a04319642732f590d
Joseph50079 <josephogbah2019@gmail.com>
2023-06-21 10:25:23 +0000

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+
+#include <stdio.h>
+#include "function_pointers.h"
+#include <string.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+#endif
