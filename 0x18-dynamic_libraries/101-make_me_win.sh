6728bb4c9814fec82eaf870029a07ad3edc02e38
Joseph50079 <josephogbah2019@gmail.com>
2023-10-17 15:18:16 -0700

    inject

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget /tmp/libinjection.so https://github.com/Joseph50079/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD=/tmp/libinjection.so
