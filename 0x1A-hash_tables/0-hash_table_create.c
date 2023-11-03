4610b3667af63ca7bb624b4bf4d6d6957d5599e8
Joseph50079 <josephogbah2019@gmail.com>
2023-11-03 13:00:18 +0000

    creat hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
+	unsigned int i;
 
-	hash->array = malloc(sizeof(hash_node_t) * size);
+	if (hash == NULL)
+	{
+		return (NULL);
+	}
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash->array == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		hash->array[i] = NULL;
 	}
 	return (hash);
 }
